95468a751b999ff002352cef7027c70f58959719
Ronak <ronakbhowmik@gmail.com>
2024-11-25 17:34:39 +0530

    added logs for debugging

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -9,6 +9,7 @@ const Projects = () => {
     useEffect(() => {
         const fetchProjects = async () => {
             try {
+                console.log(process.env.REACT_APP_BACKEND_URL);
                 const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/projects`);
                 console.log(response.data)
                 setProjects(response.data);
